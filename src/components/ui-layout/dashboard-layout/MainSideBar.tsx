b99a81a4ab40d46fcd0a311e6f0a388c0f00e59d
f.hashemian <faridhashfn@gmail.com>
2022-09-06 09:01:23 +0430

    creating structure of the side bar

---
+++ src/components/ui-layout/dashboard-layout/MainSideBar.tsx
@@ -0,0 +1,5 @@
+const MainSideBar = () => {
+  return <div>MainSideBar</div>;
+};
+
+export default MainSideBar;
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import MainSideBar from "../components/ui-layout/dashboard-layout/MainSideBar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <MainSideBar />
+    </div>
+  );
+};
+
+export default Dashboard;
